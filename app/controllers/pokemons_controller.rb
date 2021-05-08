24433a137ea2602bd9ed76ae2859fa62a2bd275a
Malakir3 <on.95.kimura.yasuyuki@gmail.com>
2021-05-08 12:10:45 +0900

    Edit new action

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,11 +1,19 @@
 class PokemonsController < ApplicationController
   def index
+    @pokemons = Pokemon.all
   end
 
   def new
+    @pokemon = Pokemon.new
   end
 
   def create
+    @pokemon = Pokemon.new(pokemon_params)
+    if @pokemon.valid?
+      Pokemon.create(pokemon_params)
+    else
+      render :new
+    end
   end
 
   def show
@@ -18,5 +26,11 @@ class PokemonsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:pokemon_number, :name, :type1, :type2)
   end
 end
