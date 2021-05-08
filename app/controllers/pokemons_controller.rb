6602fcbb6c315a0c78fe39a3a0a6ad1bc964e99b
Malakir3 <on.95.kimura.yasuyuki@gmail.com>
2021-05-08 11:40:24 +0900

    Add seven actions for pokemon controller

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,22 @@
+class PokemonsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
